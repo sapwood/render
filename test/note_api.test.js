c97b53849e39b322890b81e6407d213eaeae08b0
Brian <eyenani@gmail.com>
2024-10-16 23:23:02 -0600

    add more tests

--- test/note_api.test.js
+++ test/note_api.test.js
@@ -1,4 +1,5 @@
 const { test, after } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -10,6 +11,19 @@ test('notes are returned as json', async () => {
         .get('/api/notes')
         .expect(200)
         .expect('Content-Type',/application\/json/)
+})
+
+test('there are two notes', async () => {
+    const response = await api.get('/api/notes')
+    assert.strictEqual(response.body.length,2)
+})
+
+test('The first note is HTTP method', async () => {
+    const response = await api.get('/api/notes')
+    const contents = response.body.map(e => e.content)
+    assert(contents.includes('HTML is easy'))
+
+
 })
 
 after(async () => {
