0f251d15b80cc40d7e8a5928e9d8fd2374042ca4
Brian <eyenani@gmail.com>
2024-10-17 22:51:52 -0600

    initialize database before test

--- test/note_api.test.js
+++ test/note_api.test.js
@@ -1,10 +1,29 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
+const Note = require('../models/note')
 const api = supertest(app)
+
+const initialNotes = [
+    {
+        content : 'HTML is easy',
+        important : false
+    },
+    {
+        content : 'Browser can execute only JavaScript',
+        important : true
+    }
+]
+
+beforeEach(async () => {
+    await Note.deleteMany({})
+    let newObject = new Note(initialNotes[0])
+    await newObject.save()
+    newObject = new Note(initialNotes[1])
+    await newObject.save()
+})
 
 test('notes are returned as json', async () => {
     await api
@@ -15,7 +34,7 @@ test('notes are returned as json', async () => {
 
 test('there are two notes', async () => {
     const response = await api.get('/api/notes')
-    assert.strictEqual(response.body.length,2)
+    assert.strictEqual(response.body.length,initialNotes.length)
 })
 
 test('The first note is HTTP method', async () => {
